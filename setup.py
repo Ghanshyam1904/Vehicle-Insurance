99099ec706eba553a72a62b98e32328ce6bc4a3a
Ghanshyam <ghanshyampatil2002@gmail.com>
2025-11-13 22:47:31 +0530

    'project-structure-ready'

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name="Vehicle Insurance",
+    version="0.0.4",
+    author="Ghanshyam",
+    author_email="ghanshyampatil2002@gmail.com",
+    packages=find_packages()
+)
